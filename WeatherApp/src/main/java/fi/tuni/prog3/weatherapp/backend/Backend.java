dbff16c1e390488dc7d77b2cc0170bc71190bb72
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-04-08 22:39:12 +0300

    Started the actual backend coding

---
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/backend/Backend.java
@@ -0,0 +1,18 @@
+package fi.tuni.prog3.weatherapp.backend;
+
+import fi.tuni.prog3.weatherapp.backend.io.iReadAndWriteToFile;
+
+public class Backend implements iReadAndWriteToFile {
+
+
+    @Override
+    public String readFromFile(String fileName) throws Exception {
+        return null;
+    }
+
+
+    @Override
+    public boolean writeToFile(String fileName) throws Exception {
+        return false;
+    }
+}
